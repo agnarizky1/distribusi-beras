30428e0927d5d2d7713b09dd9c52a4d2aad71dc4
NaafiRamadhan <zidannaaafi@gmail.com>
2023-11-10 09:32:01 +0700

    faker

--- database/seeders/TokoSeeder.php
+++ database/seeders/TokoSeeder.php
@@ -10,42 +10,32 @@ class TokoSeeder extends Seeder
 {
     public function run()
     {
-        $nm_toko = ['joyo', 'subur', 'makmur', 'sumber', 'rejeki', 'abadi', 'abadi', 'abdul', 'sari', 'tukul', 'jembar'];
-        $pemilik = ['Joko', 'Nuha', 'Tun', 'Bon', 'Vander', 'Ado', 'Edi', 'Akil', 'Nimo', 'Senlong', 'Jupri'];
-        $alamat = ['Sumbersari', 'Jl. Kalimantan', 'Jl. Sumatra', 'Kemiren', 'Rogojampi', 'Blimbingsari'];
+    $faker = Faker::create('id_ID');
 
-        for ($i = 0; $i < 100; $i++) {
-            $prefix = 'T-';
-            $lastId = Toko::max('id_toko');
+    for ($i = 0; $i < 100; $i++) {
+        $prefix = 'T-';
+        $lastId = Toko::max('id_toko');
 
-            if (!$lastId) {
-                // Jika belum ada data, gunakan nomor 1
-                $nextId = $prefix . '00001';
-            } else {
-                // Ambil angka dari ID terakhir, tambahkan 1, dan lakukan padding
-                $lastNumber = intval(substr($lastId, strlen($prefix))) + 1;
-                $nextId = $prefix . str_pad($lastNumber, 5, '0', STR_PAD_LEFT);
-            }
-
-            $tambah = [
-                'id_toko' => $nextId,
-                'nama_toko' => $nm_toko[mt_rand(0, 10)] . ' ' . $nm_toko[mt_rand(0, 10)],
-                'grade_toko' => 'BIASA',
-                'pemilik' => $pemilik[mt_rand(0, 10)],
-                'alamat' => $alamat[mt_rand(0, 5)],
-                'nomor_tlp' => '0812' . mt_rand(10000000, 99999999),
-            ];
-
-            Toko::insert($tambah);
-            // $data[] = $tambah;
+        if (!$lastId) {
+            // Jika belum ada data, gunakan nomor 1
+            $nextId = $prefix . '00001';
+        } else {
+            // Ambil angka dari ID terakhir, tambahkan 1, dan lakukan padding
+            $lastNumber = intval(substr($lastId, strlen($prefix))) + 1;
+            $nextId = $prefix . str_pad($lastNumber, 5, '0', STR_PAD_LEFT);
         }
 
-        // foreach ($data as $item) {
-        //     echo 'ID Toko: ' . $item['id_toko'] . ', Nama Toko: ' . $item['nama_toko'] . PHP_EOL;
-        // }
-
-        // Masukkan data ke tabel 'tokos'
-        // Toko::insert($data);
+        \DB::table('tokos')->insert([
+            'id_toko' => $nextId,
+            'nama_toko' => $faker->company,
+            'grade_toko' => 'BIASA',
+            'pemilik' => $faker->name,
+            'alamat' => $faker->address,
+            'nomor_tlp' => $faker->phoneNumber,
+            // Add other columns as needed
+        ]);
+     }
     }
+
 }
 
